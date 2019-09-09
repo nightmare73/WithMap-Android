2cc76ab56bfbb858b67f983c20465722b5569762
vitriol2 <vitriol0919@gmail.com>
2019-09-09 11:46:30 +0900

    SignUpFragment react finished

--- app/src/main/java/com/ebookfrenzy/withmap/view/login/SignUpFragment.kt
+++ app/src/main/java/com/ebookfrenzy/withmap/view/login/SignUpFragment.kt
@@ -2,32 +2,110 @@ package com.ebookfrenzy.withmap.view.login
 
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.lifecycle.MutableLiveData
 import com.ebookfrenzy.withmap.R
+import com.ebookfrenzy.withmap.databinding.FragmentSignUpBinding
+import com.ebookfrenzy.withmap.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SignUpFragment : Fragment() {
+//
+//    private lateinit var binding : FragmentSignUpBinding
 
-    private lateinit var
+    private val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+//
+//        binding = FragmentSignUpBinding.inflate(LayoutInflater.from(context))
         return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        repeatButtonListener()
+
+    }
+    fun repeatButtonListener() {
+        et_email.addTextChangedListener( object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s!!.isNotEmpty()) {
+                    btn_email_repeat.setBackgroundResource(R.drawable.bg_button_repeat_focus)
+                    if(isValidEmail(s)){
+                        tv_frag_sign_up_email_form.visibility = View.GONE
+                    }else
+                        tv_frag_sign_up_email_form.visibility = View.VISIBLE
+                }else{
+                    btn_email_repeat.setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+                }
+            }
+        })
+        et_nickname.addTextChangedListener( object : TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s!!.isNotEmpty()) {
+                    btn_nickname_repeat.setBackgroundResource(R.drawable.bg_button_repeat_focus)
+                }else{
+                    btn_nickname_repeat.setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+                }
+            }
+        })
+    }
+    fun isValidEmail(target: CharSequence?): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches()
 
     }
 
-
+//    fun repeatButtonListener(i : Int) {
+//        when(i) {
+//            1 -> if(binding.etEmail.text.isNotEmpty()) {
+//                binding.btnEmailRepeat.setBackgroundResource(R.drawable.bg_button_repeat_focus)
+//            }else {
+//                binding.btnEmailRepeat.setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+//            }
+//
+//            2 -> if(binding.etNickname.text.isNotEmpty()) {
+//                binding.btnNicknameRepeat.setBackgroundResource(R.drawable.bg_button_repeat_focus)
+//            }else {
+//                binding.btnNicknameRepeat.setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+//            }
+//        }
+//    }
+//
+//    @BindingAdapter("is_repeat")
+//    fun Button.setButton(isText : Boolean) {
+//        if(isText) {
+//            setBackgroundResource(R.drawable.bg_button_repeat_focus)
+//        } else{
+//            setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+//        }
+//    }
 }
--- app/src/main/java/com/ebookfrenzy/withmap/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/ebookfrenzy/withmap/viewmodel/LoginViewModel.kt
@@ -1,8 +1,30 @@
 package com.ebookfrenzy.withmap.viewmodel
 
+import android.widget.Button
+import android.widget.EditText
+import androidx.databinding.BindingAdapter
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import com.ebookfrenzy.withmap.R
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel (mEtEmailText : Boolean = false, mEtNicknameText : Boolean = false){
 
+    val etEmailText = ObservableField<String>()
+    val etNicknameText = ObservableField<String>()
 
+}
+
+@BindingAdapter(value=["textFrom", "etEmailOrNickname"])
+fun etIsText(
+    view : Button,
+    textFrom : String,
+    etEmailOrNickname : Int
+){
+    when(etEmailOrNickname) {
+        1 -> if(textFrom.isNotEmpty()) {
+            view.setBackgroundResource(R.drawable.bg_button_repeat_focus)
+        } else{
+            view.setBackgroundResource(R.drawable.bg_button_repeat_not_focus)
+        }
+    }
 }
