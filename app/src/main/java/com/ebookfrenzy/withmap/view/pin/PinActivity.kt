8456a8b72f225a9a395f0a0bc43c31c7d37e3309
vitriol2 <vitriol0919@gmail.com>
2019-09-10 17:26:21 +0900

    color.xml resolve

---
+++ app/src/main/java/com/ebookfrenzy/withmap/view/pin/PinActivity.kt
@@ -0,0 +1,13 @@
+package com.ebookfrenzy.withmap.view.pin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ebookfrenzy.withmap.R
+
+class PinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pin)
+    }
+}
