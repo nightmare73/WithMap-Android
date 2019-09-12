c580b312c8d0a22bb67a6dd1d662e7933a6ff8eb
Malibin <nightmare73@naver.com>
2019-09-12 23:28:44 +0900

    검색 뷰 레이아웃 완성 및 Koin base 파일 생성, 다음 api 인터페이스 작성완료

---
+++ app/src/main/java/com/ebookfrenzy/withmap/config/DiModule.kt
@@ -0,0 +1,14 @@
+package com.ebookfrenzy.withmap.config
+
+import org.koin.dsl.module
+
+/**
+ * Created By Yun Hyeok
+ * on 9월 12, 2019
+ */
+
+val diModules = listOf(-1)
+
+val apiModule = module {
+
+}
