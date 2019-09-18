2e7ed252c6443fd68e2ac9d590a112ba25936220
Malibin <nightmare73@naver.com>
2019-09-18 21:47:30 +0900

    Local Repository, SharedPreference 추가. 이걸로 처음 들어온사람만 도움말 화면 나옴

---
+++ app/src/main/java/com/ebookfrenzy/withmap/respository/LocalRepository.kt
@@ -0,0 +1,6 @@
+package com.ebookfrenzy.withmap.respository
+
+/**
+ * Created By Yun Hyeok
+ * on 9월 18, 2019
+ */
---
+++ app/src/main/java/com/ebookfrenzy/withmap/respository/SharedPreferenceSource.kt
@@ -0,0 +1,12 @@
+package com.ebookfrenzy.withmap.respository
+
+import android.content.Context
+
+/**
+ * Created By Yun Hyeok
+ * on 9월 18, 2019
+ */
+
+class SharedPreference(private val context: Context) {
+
+}
