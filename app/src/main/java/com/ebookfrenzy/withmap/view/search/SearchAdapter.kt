7a98a12070b88bffe95e8914c6e5ef99edd7112b
Malibin <nightmare73@naver.com>
2019-09-20 23:00:32 +0900

    검색 리사이클러뷰 어댑터 어느정도 작성

--- app/src/main/java/com/ebookfrenzy/withmap/view/search/SearchAdapter.kt
+++ app/src/main/java/com/ebookfrenzy/withmap/view/search/SearchAdapter.kt
@@ -1,16 +1,60 @@
 package com.ebookfrenzy.withmap.view.search
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.ebookfrenzy.withmap.data.SearchLocationResult
+import com.ebookfrenzy.withmap.databinding.RvItemSearchBinding
 
 /**
  * Created By Yun Hyeok
  * on 9월 20, 2019
  */
 
-//class SearchAdapter : RecyclerView.Adapter<>() {
-//
-//
-//    class ViewHolder() : RecyclerView.ViewHolder {
-//
-//    }
-//}
+class SearchAdapter : ListAdapter<SearchLocationResult, SearchAdapter.ViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            RvItemSearchBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val location = getItem(position)
+        holder.apply {
+            bind(location)
+            itemView.tag = location
+        }
+    }
+
+
+    class ViewHolder(private val binding: RvItemSearchBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(location: SearchLocationResult) {
+            binding.location = location
+        }
+    }
+
+}
+
+private class DiffCallback : DiffUtil.ItemCallback<SearchLocationResult>() {
+
+    override fun areItemsTheSame(
+        oldItem: SearchLocationResult,
+        newItem: SearchLocationResult
+    ): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(
+        oldItem: SearchLocationResult,
+        newItem: SearchLocationResult
+    ): Boolean {
+        return oldItem == newItem
+    }
+}
